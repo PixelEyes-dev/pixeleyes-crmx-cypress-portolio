40c60d303c7b3df502dc85de7cb5077fd752d13e
pixeleyes27 <126045087+pixeleyes27@users.noreply.github.com>
2025-06-19 12:58:18 -0600

    verify github workflow with dummy test

---
+++ cypress/e2e/workflowVerification/githubWorkflow1.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe("User Login", () => {
+  it("should authenticate user", () => {
+    cy.login();
+    cy.get('a[href="/customers"]').click();
+    cy.get("h1").should("have.text", "Customers");
+  });
+});
